434fba9898cbf00033e8735b532e73cd5f49e659
MotokiAdachi <m.0622.kiki@gmail.com>
2026-01-03 15:34:05 +0900

    learning

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json([
+    {
+      id: 1,
+      name: "Yamada",
+    },
+    {
+      id: 2,
+      name: "Tanaka",
+    },
+  ]);
+}
