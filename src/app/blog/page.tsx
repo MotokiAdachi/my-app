bdc6b179929c0e5d54d7e8653eb9614ed1578de4
MotokiAdachi <m.0622.kiki@gmail.com>
2026-01-03 15:24:03 +0900

    learning

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,36 @@
+import { resolve } from "path";
+
+// ダミーデータ
+const articles = [
+  {
+    id: "1",
+    title: "タイトル1",
+  },
+  {
+    id: "2",
+    title: "タイトル2",
+  },
+  {
+    id: "3",
+    title: "タイトル3",
+  },
+];
+
+// 3秒待機設定
+async function fetchArticles() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return articles;
+}
+
+export default async function page() {
+  const articles = await fetchArticles();
+  return (
+    <div>
+      <ul>
+        {articles.map((articles) => (
+          <li key={articles.id}>{articles.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
